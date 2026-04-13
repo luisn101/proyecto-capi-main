97c723ee9f66ee0d6a36cdd272d03ff9744831ae
Luis Navia <luisnt1202@gmail.com>
2026-04-13 17:17:28 -0400

    add calendar to galeria page, fix navbar scroll behavior, and update styles for better spacing and layout.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,15 @@
+const { DateTime } = require("luxon");
+
 module.exports = function (eleventyConfig) {
+  // --- EL FILTRO DE FECHA ---
+  eleventyConfig.addFilter(
+    "dateFilter",
+    (dateObj, format = "dd LLL", lang = "es") => {
+      // Convertimos el texto del JSON a un objeto de fecha y luego al formato que queramos
+      // 'dd' es el día (15) y 'LLL' es el mes abreviado (MAY)
+      return DateTime.fromISO(dateObj).setLocale(lang).toFormat(format);
+    },
+  );
   // Copiar carpetas de assets al destino final sin procesar
   eleventyConfig.addPassthroughCopy("src/assets");
   // Si tienes el CSS o JS en carpetas separadas en la raíz de src, añádelas:
